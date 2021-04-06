a762648f6c89d2d99d48ae9fcec1035d1ddad788
ANA KAROLINA SANTOS OLIVEIRA <anna15soliveira@gmail.com>
2021-04-06 11:57:09 -0300

    OrderItem, many-to-many association with extra attributes = PART 2

---
+++ src/main/java/com/example/ProjetoWebServices/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.ProjetoWebServices.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ProjetoWebServices.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
