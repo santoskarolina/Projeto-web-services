e7f99d8d41a80c6534ce0ef6ebd3789192c56739
ANA KAROLINA SANTOS OLIVEIRA <anna15soliveira@gmail.com>
2021-04-05 21:01:35 -0300

    Project created

---
+++ src/main/java/com/example/ProjetoWebServices/ProjetoWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesApplication.class, args);
+	}
+
+}
